1921d3ceca50a32d7c1120dffc012e967bd9eebe
Veronika <veronika.jefimova03@gmail.com>
2024-10-15 10:00:25 +0300

    setup

---
+++ src/ToDoList.jsx
@@ -0,0 +1,30 @@
+import React, { useState } from 'react'
+
+function ToDoList(){
+
+    const [tasks, setTasks] = useState([]);
+    const [newTask, setNewTask] = useState("");
+
+    function handleInputChange(event){
+
+    }
+
+    function addTask(){
+
+    }
+
+    function deleteTask(index){
+
+    }
+
+    function moveTaskUp(index){
+
+    }
+
+    function moveTaskDown(index){
+
+    }
+
+    return(<></>)
+}
+export default ToDoList
