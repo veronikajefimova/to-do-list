0a5358dea674bfca4d1aef0a104eb168e28fa7bf
Veronika <veronika.jefimova03@gmail.com>
2024-10-15 11:48:37 +0300

    Add HTML for to do list

--- src/ToDoList.jsx
+++ src/ToDoList.jsx
@@ -6,7 +6,7 @@ function ToDoList(){
     const [newTask, setNewTask] = useState("");
 
     function handleInputChange(event){
-
+        setNewTask(event.target.value);
     }
 
     function addTask(){
@@ -25,6 +25,27 @@ function ToDoList(){
 
     }
 
-    return(<></>)
+    return(
+    <div className='to-do-list'>
+
+        <h1>To Do List</h1>
+
+        <div>
+            <input type="text" placeholder="Enter a task..." value={newTask} onChange={handleInputChange} name="" id="" />
+            <button className='add-button' onClick={addTask}>Add</button>
+        </div>
+
+        <ol>
+            {tasks.map((task, index) => 
+                <li key={index}>
+                    <span className='text'>{task}</span>
+                    <button className='delete-button' onClick={() => deleteTask(index)}>Delete</button>
+                    <button className='move-button' onClick={() => moveTaskUp(index)}>UP</button>
+                    <button className='move-button' onClick={() => moveTaskDown(index)}>DOWN</button>
+                </li>
+            )}
+        </ol>
+
+    </div>)
 }
 export default ToDoList
