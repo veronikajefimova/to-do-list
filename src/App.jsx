b85381054474b6e008e390ff50e7cde07267c87f
Veronika <veronika.jefimova03@gmail.com>
2024-10-23 01:00:43 +0300

    Add ThemeToggler

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,35 @@
 import './App.css'
 import ToDoList from './ToDoList'
 import Header from './Header'
+import { ThemeProvider, useTheme } from './ThemeContext'
+import { width } from '@mui/system';
+
+const ThemedApp = () => {
+  const { theme } = useTheme(); // Получаем текущую тему
+  console.log('theme: ', theme);
+
+  const appStyle = {
+    backgroundColor: theme === 'light' ? '#fff' : '#242424',
+    color: theme === 'light' ? '#000' : '#fff',
+    height: '100vh',
+    width: '100vw'
+  };
+
+  return (
+    <div style={appStyle}>
+      <Header/>
+      <ToDoList/>
+    </div>
+  );
+};
 
 function App() {
 
   return(
-  <>
-    <Header/>
-    <ToDoList/>
-  </>)
+    <ThemeProvider>
+      <ThemedApp />
+    </ThemeProvider>
+    )
 }
 
 export default App
