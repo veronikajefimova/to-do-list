a90d52999367e098265e2d7263f59bc4180155db
Veronika <veronika.jefimova03@gmail.com>
2024-10-22 13:51:14 +0300

    Add menu

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,14 @@
 import './App.css'
 import ToDoList from './ToDoList'
+import Header from './Header'
 
 function App() {
 
-  return(<ToDoList/>)
+  return(
+  <>
+    <Header/>
+    <ToDoList/>
+  </>)
 }
 
 export default App
